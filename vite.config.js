357672fd2f81df0c2f2716456eefa68425d9d4ec
webferrol <xurxo@webferrol.com>
2023-07-04 14:54:36 +0200

    docs 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/react-login-firebase/',
   plugins: [react()]
 })
