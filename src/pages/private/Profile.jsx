f9800615b36a525e4decd56cdfeb8febf5d6bc3b
webferrol <xurxo@webferrol.com>
2023-07-05 16:21:36 +0200

    feature createObjectURL

--- src/pages/private/Profile.jsx
+++ src/pages/private/Profile.jsx
@@ -1,15 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { ContainerForm } from '../../layouts/ContainerForm'
 import { UserContext } from '../../context/UserContext'
 
 export function Profile () {
   const { email, displayName } = useContext(UserContext)
+  const [image, setImage] = useState(null)
+  const inputFileRef = useRef()
   const handleSubmit = e => {
     e.preventDefault()
+  }
+
+  const cleanUp = () => {
+    URL.revokeObjectURL(image)
+    inputFileRef.current.value = null
+  }
+
+  const handleChange = e => {
+    cleanUp()
+    const newImage = e.target.files[0]
+    if (newImage) {
+      setImage(URL.createObjectURL(newImage))
+    }
   }
   return (
     <ContainerForm title='Perfil de usuario' onSubmit={handleSubmit}>
       Email: {email} - {displayName}
+      <input type='file' onChange={handleChange} ref={inputFileRef} />
+      {image}
+      <img src={image}  alt="" />
     </ContainerForm>
   )
 }
