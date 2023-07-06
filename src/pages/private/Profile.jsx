16fd822102ea3886187824efc337defa72ee5d1d
webferrol <xurxo@webferrol.com>
2023-07-06 12:51:53 +0200

    feature handleFile

--- src/pages/private/Profile.jsx
+++ src/pages/private/Profile.jsx
@@ -16,17 +16,34 @@ export function Profile () {
     _updateProfile(data)
   }
 
+  const upload = async file => {
+    const formData = new FormData()
+    formData.append('file', file)
+    formData.append('upload_preset', 'ngp3nkgu')
+    formData.append('api_key', '153898983155635')
+    const response = await fetch(
+      'https://api.cloudinary.com/v1_1/dimvf1zl2/image/upload',
+      {
+        method: 'POST',
+        body: formData
+      }
+    )
+    const fileData = await response.json()
+    console.log(fileData)
+    const { url } = fileData
+  }
+
   const cleanUp = () => {
     URL.revokeObjectURL(image)
+    inputFileRef.current.value = ''
     setImage(null)
   }
 
-  const handleChange = e => {
-
-    // const newImage = e.target.files[0]
-    // if (newImage) {
-    //   setImage(URL.createObjectURL(newImage))
-    // }
+  const handleFile = ({ target }) => {
+    const newImage = target.files[0]
+    if (newImage) {
+      setImage(URL.createObjectURL(newImage))
+    } else cleanUp()
   }
 
   if (isLoading) {
@@ -49,38 +66,41 @@ export function Profile () {
     )
   }
   return (
-    <ContainerForm title='Perfil de usuario' onSubmit={handleSubmit}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={12}>
-          <TextField
-            autoComplete='given-name'
-            name='displayName'
-            fullWidth
-            id='displayName'
-            label='Nombre a mostrar'
-            autoFocus
-            value={displayName}
-            onChange={onChange}
-          />
+    <>
+      <ContainerForm title='Perfil de usuario' onSubmit={handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12}>
+            <TextField
+              autoComplete='given-name'
+              name='displayName'
+              fullWidth
+              id='displayName'
+              label='Nombre a mostrar'
+              autoFocus
+              value={displayName}
+              onChange={onChange}
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              type='submit'
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Enviar
+            </Button>
+            {error && <Alert severity='error'>{error.message}</Alert>}
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Enviar
-          </Button>
-          {error && <Alert severity='error'>{error.message}</Alert>}
-        </Grid>
-      </Grid>
-      {/* <input name='photoURL' type='file' onChange={handleChange} ref={inputFileRef} /> */}
-      <button type='button' onClick={cleanUp}>Limpiar</button>
-      {image}
-      <img src={image} alt='' />
-      {inputFileRef.current?.value}
-      {/* <UploadWidget /> */}
-    </ContainerForm>
+        <button type='button' onClick={cleanUp}>Limpiar</button>
+        {image}
+        <img src={image} alt='' style={{ maxWidth: '50ch' }} />
+        {/* <UploadWidget /> */}
+      </ContainerForm>
+      <input name='photoURL' type='file' onChange={handleFile} ref={inputFileRef} />
+    </>
   )
 }
+
+// https://res.cloudinary.com/dimvf1zl2/image/upload/f_webp/v1688639315/images/mmhl5lczvgyf2cfzbpu2.gif
